f69aa3332dd406ffeb9781dde18879aa84e997c6
Bedrich Hovorka <bedrich.hovorka@gmail.com>
2026-02-05 16:05:35 +0100

    backup

--- src/test/kotlin/cz/vutbr/fit/interlockSim/gui/gridcanvas/CellRendererTest.kt
+++ src/test/kotlin/cz/vutbr/fit/interlockSim/gui/gridcanvas/CellRendererTest.kt
@@ -10,6 +10,7 @@
 package cz.vutbr.fit.interlockSim.gui.gridcanvas
 
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
 import cz.vutbr.fit.interlockSim.objects.cells.DynamicInOut
 import cz.vutbr.fit.interlockSim.objects.cells.DynamicRailSwitch
